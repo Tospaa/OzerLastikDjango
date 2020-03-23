be65bfbbbb677bd77035fd8edb4b706c385efc64
Tospaa <musaecer@gmail.com>
2020-03-23 08:38:49 +0300

    changing views for prod
    personal information removed

--- tsp_prj/settings.py
+++ tsp_prj/settings.py
@@ -31,7 +31,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['www.ozerlastik.com']
 
 # Application definition
 
